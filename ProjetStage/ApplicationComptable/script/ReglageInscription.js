79e8705885e526d9a6d87e658de4c63b6d3ee186
LEFEBVRE Kevin <lefebvrekevin2011@gmail.com>
2022-07-18 17:02:19 +0200

    ApplicationComptable

---
+++ ProjetStage/ApplicationComptable/script/ReglageInscription.js
@@ -0,0 +1,27 @@
+let form = document.querySelector('#email22');
+//Entendre la modification de cet Email
+
+form.email.addEventListener('change', function () {
+    validerEmail(this);
+});
+
+const validerEmail = function (inputEmail) {
+    //Création de la regex pour vérifier notre email
+    let emailRegExp = new RegExp(
+        '^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g'
+    );
+};
+
+let testeremail = emailRegExp.test(inputEmail.value);
+let petit = inputEmail.nextElementSibling;
+
+if (testeremail) {
+    petit.innerHTML = 'Email OK';
+    petit.classList.remove('text-danger');
+    petit.classList.add('text-success');
+} else {
+    petit.innerHTML = 'Email incorrect';
+    petit.classList.remove('text-success');
+    petit.classList.add('text-danger');
+}
+console.log(petit);
